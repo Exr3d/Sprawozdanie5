105e55d1bbc33e7c9491b4a04c040d52d5e19521
Exred <tymonwoj57@gmail.com>
2022-01-28 18:21:21 +0100

    Added Axios to react and django cors to django API

--- sprawozdanie5/src/App.js
+++ sprawozdanie5/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
+
+const getFromApi = () => {
+  Axios.get("http://127.0.0.1:8000/KartyGraficzne/api/karty/?format=json").then((response) => {
+    console.log(response);
+  })
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ getFromApi }>POBIERZ</button>
     </div>
   );
 }
