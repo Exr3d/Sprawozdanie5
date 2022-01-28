e6960a7038e932eb41468b5b4def4311ea553bfe
Exred <tymonwoj57@gmail.com>
2022-01-28 19:38:39 +0100

    Added toast notification

--- sprawozdanie5/src/App.js
+++ sprawozdanie5/src/App.js
@@ -1,17 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios'
+import png1060 from './1060.png';
+import png1070 from './1070.jpg';
+import png2060 from './2060.jpg';
+import png2070 from './2070.jpg';
+import png3060 from './3060.jpg';
+import png3070 from './3070.jpg'
+import png3080 from './3080.jpg';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
+toast.configure()
 const getFromApi = () => {
   Axios.get("http://127.0.0.1:8000/KartyGraficzne/api/karty/?format=json").then((response) => {
-    console.log(response);
+    var x =0
+    let informacje = []
+    while (x < response.data.length) {
+        informacje[x] = response.data[x].id+ " "+ response.data[x].Marka + " " + response.data[x].Model + " "+response.data[x].VRAM+ " "+ response.data[x].avgCena
+        console.log(informacje[x]);
+        x++; // increment
+      
+    }
+    
+    
   })
 }
 
+
+
 function App() {
+  let informacje = []
+  
+    Axios.get("http://127.0.0.1:8000/KartyGraficzne/api/karty/?format=json").then((response) => {
+    var x =0
+    //let informacje = []
+    while (x < response.data.length) {
+        informacje[x] = "Karta "+ response.data[x].Marka + " " + response.data[x].Model + ", ilość vramu: "+response.data[x].VRAM+ ", średnia cena: "+ response.data[x].avgCena
+        console.log(informacje[x]);
+        
+      x++; // increment
+      
+    }
+    
+    
+  })
+  
+  
+  let info;
   return (
+    
     <div className="App">
-      <button onClick={ getFromApi }>POBIERZ</button>
+      <div className="imgs">
+        <img src={ png1070 } onClick = {() => { 
+          toast.info(informacje[0])
+        }}/>
+        <img src={ png1060 } onClick = {() => { 
+          toast.info(informacje[1])
+        }}/>
+        <img src={ png2060 } onClick = {() => { 
+          toast.info(informacje[2])
+        }}/>
+        <img src={ png2070 } onClick = {() => { 
+          toast.info(informacje[3])
+        }}/>
+        <img src={ png3060 } onClick = {() => { 
+          toast.info(informacje[4])
+        }}/>
+        <img src={ png3070 } onClick = {() => { 
+          toast.info(informacje[5])
+        }}/>
+        <img src={ png3080 } onClick = {() => { 
+          toast.info(informacje[6])
+        }}/>
+      </div>
     </div>
   );
 }
